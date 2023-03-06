6958cc36be1e9c68c425525e12e8c539f18f78a8
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2023-03-06 20:59:12 +0300

    fixed navigation

--- app/src/main/java/com/example/pakuganda/MainActivity.java
+++ app/src/main/java/com/example/pakuganda/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.pakuganda;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView navView;
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpNavigation();
+    }
+
+    private void setUpNavigation() {
+        navView = findViewById(R.id.bottomNav_view);
+        navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }
