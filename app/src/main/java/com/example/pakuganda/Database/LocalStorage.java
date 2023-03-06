6bbbb2b19dc2f0d0336ab9eca16d7a8ca35ea0c8
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2023-03-06 23:33:51 +0300

    update on app

---
+++ app/src/main/java/com/example/pakuganda/Database/LocalStorage.java
@@ -0,0 +1,251 @@
+package com.example.pakuganda.Database;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LocalStorage extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "Mwonya";
+    private static final int DB_VERSION = 3;
+    private static final String TABLE_TRACK = "track";
+    private static final String TABLE_ArtistFollowing = "ArtistFollowing";
+    private static final String TABLE_LIKED_TRACK = "liked_tracks";
+    public static final String COLUMN_id = "_id";
+    public static final String COLUMN_trackId = "trackId";
+    public static final String COLUMN_trackTitle = "trackTitle";
+    public static final String COLUMN_trackArtist = "trackArtist";
+    public static final String COLUMN_trackArtistID = "trackArtistID";
+    public static final String COLUMN_trackAlbum = "trackAlbum";
+    public static final String COLUMN_trackArtworkPath = "trackArtworkPath";
+    public static final String COLUMN_trackGenre = "trackGenre";
+    public static final String COLUMN_trackGenreID = "trackGenreID";
+    public static final String COLUMN_trackDuration = "trackDuration";
+    public static final String COLUMN_trackPath = "trackPath";
+    public static final String COLUMN_trackTotalPlays = "trackTotalPlays";
+    public static final String COLUMN_trackWeeklyPlays = "trackWeeklyPlays";
+    public static final String COLUMN_track_status = "track_status";
+    public static final String COLUMN_track_lastPlayed = "track_lastPlayed";
+    public static final String COLUMN_track_user_plays = "track_user_plays";
+    public static final String COLUMN_artistFollow_status = "status";
+    public static final String COLUMN_UserID = "userID";
+    public static final String COLUMN_ArtistID = "artistID";
+    public static final String COLUMN_FollowDate = "followDate";
+    public static final int NOT_SYNCED_WITH_SERVER = 0;
+    public static final int SYNCED_WITH_SERVER = 1;
+
+
+
+    public LocalStorage(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        //create CART table
+        String sql = "CREATE TABLE " + TABLE_LIKED_TRACK + " (" +
+                COLUMN_id + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_trackId + " INTEGER," +
+                COLUMN_track_status + " INTEGER DEFAULT " + NOT_SYNCED_WITH_SERVER + " )";
+
+        db.execSQL(sql);
+
+        //create Track table
+        String recently_played_sql = "CREATE TABLE " + TABLE_TRACK + " (" +
+                COLUMN_id + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_trackId + " INTEGER," +
+                COLUMN_trackTitle + " TEXT," +
+                COLUMN_trackArtist + " TEXT," +
+                COLUMN_trackArtistID + " INTEGER," +
+                COLUMN_trackAlbum + " TEXT," +
+                COLUMN_trackArtworkPath + " TEXT," +
+                COLUMN_trackGenre + " TEXT," +
+                COLUMN_trackGenreID + " INTEGER," +
+                COLUMN_trackDuration + " TEXT," +
+                COLUMN_trackPath + " TEXT," +
+                COLUMN_trackTotalPlays + " INTEGER," +
+                COLUMN_trackWeeklyPlays + " TEXT," +
+                COLUMN_track_lastPlayed + " DATETIME DEFAULT (datetime('now','localtime'))," +
+                COLUMN_track_user_plays + " INTEGER DEFAULT 0 ," +
+                COLUMN_track_status + " INTEGER DEFAULT " + NOT_SYNCED_WITH_SERVER + " )";
+
+        db.execSQL(recently_played_sql);
+
+        //artistFollowing
+        String SQL_CREATE_ARTIST_FOLLOW_TABLE = "CREATE TABLE " + TABLE_ArtistFollowing + " ("
+                + COLUMN_id + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_UserID + " TEXT NOT NULL, "
+                + COLUMN_ArtistID + " TEXT NOT NULL, "
+                + COLUMN_FollowDate + " DATETIME DEFAULT (datetime('now','localtime')),"
+                + COLUMN_artistFollow_status + " INTEGER DEFAULT " + NOT_SYNCED_WITH_SERVER + ");";
+
+        db.execSQL(SQL_CREATE_ARTIST_FOLLOW_TABLE);
+    }
+
+    //upgrading the database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TRACK);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_LIKED_TRACK);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ArtistFollowing);
+        onCreate(db);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public boolean checkTrackInDb(String id_str) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_TRACK,
+                new String[]{COLUMN_trackId},
+                COLUMN_trackId + " = ?",
+                new String[]{id_str},
+                null, null, null, null);
+        if (cursor.moveToFirst()) {
+            //recordexist
+            cursor.close();
+            return false;
+        } else {
+            //record not existing
+            cursor.close();
+
+            return true;
+        }
+    }
+
+
+
+    public void addLikedTrack(String trackID, int track_status) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_trackId, trackID);
+        values.put(COLUMN_track_status, track_status);
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.insert(TABLE_LIKED_TRACK, null, values);
+    }
+
+    public void removeLikedTrack(String trackID, int track_status) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_LIKED_TRACK, COLUMN_trackId + " = ?", new String[]{trackID});
+    }
+
+    public boolean checkLikedTrackID(String id_str) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_LIKED_TRACK,
+                new String[]{COLUMN_trackId},
+                COLUMN_trackId + " = ?",
+                new String[]{id_str},
+                null, null, null, null);
+        if (cursor.moveToFirst()) {
+            //recordexist
+            cursor.close();
+            return false;
+        } else {
+            //record not existing
+            cursor.close();
+
+            return true;
+        }
+    }
+
+
+    //update Database
+    public void updateDataRecentlyPlayed(String trackID, int syncStatus) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_trackId, trackID);
+        values.put(COLUMN_track_status, syncStatus);
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.update(TABLE_TRACK, values, COLUMN_trackId + " = ?", new String[]{trackID});
+    }
+
+    //update LikedSong Database
+    public void updateDataLikedSongPlayed(String trackID, int syncStatus) {
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_trackId, trackID);
+        values.put(COLUMN_track_status, syncStatus);
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.update(TABLE_LIKED_TRACK, values, COLUMN_trackId + " = ?", new String[]{trackID});
+    }
+
+    public void clearDatabases() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_LIKED_TRACK, null, null);
+        db.delete(TABLE_TRACK, null, null);
+        db.close();
+    }
+
+
+    //follow_not_followArtist
+    public void followUnfollowArtist(String userId, String artistId, int status, boolean follow) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_UserID, userId);
+        values.put(COLUMN_ArtistID, artistId);
+        values.put(COLUMN_artistFollow_status, status);
+
+        if (follow) {
+            // Insert the new row, returning the primary key value of the new row
+            long newRowId = db.insert(TABLE_ArtistFollowing, null, values);
+        } else {
+            // Delete the artist from the list
+            String selection = COLUMN_UserID + " = ? AND " + COLUMN_ArtistID + " = ?";
+            String[] selectionArgs = {userId, artistId};
+            db.delete(TABLE_ArtistFollowing, selection, selectionArgs);
+        }
+    }
+
+    public boolean isFollowingArtist(String userId, String artistId) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // projection to get the Artist ID from the DB
+        String[] projection = {
+                COLUMN_id
+        };
+
+        String selection = COLUMN_UserID + " = ? AND " +
+                COLUMN_ArtistID + " = ?";
+        String[] selectionArgs = {userId, artistId};
+
+        Cursor cursor = db.query(
+                TABLE_ArtistFollowing,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                null               // The sort order
+        );
+
+        if(cursor.getCount() > 0){
+            cursor.close();
+            return true;
+        }
+        cursor.close();
+        return false;
+    }
+
+
+}
--- app/src/main/java/com/example/pakuganda/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/pakuganda/Fragments/SettingsFragment.java
@@ -1,66 +1,153 @@
 package com.example.pakuganda.Fragments;
 
+import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
+
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory;
+import com.example.pakuganda.Database.LocalStorage;
+import com.example.pakuganda.HelperClasses.SharedPrefManager;
+import com.example.pakuganda.LoginMaterial;
+import com.example.pakuganda.MalUtils.NetworkStatusIntentService;
+import com.example.pakuganda.Models.UserModel;
+import com.example.pakuganda.NetworkReceiver;
+import com.example.pakuganda.NetworkStateChecker;
 import com.example.pakuganda.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 public class SettingsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    LocalStorage db;
+    TextView textViewUsername, textViewEmail, other_info;
+    ImageView profile_image;
+    ImageButton backbtn;
+    private DrawableCrossFadeFactory factory;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        if (!SharedPrefManager.getInstance(getContext()).isLoggedIn()) {
+            startActivity(new Intent(getContext(), LoginMaterial.class));
+        }
+        factory = new DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build();
+
+        textViewUsername = (TextView) view.findViewById(R.id.full_name);
+        profile_image = (ImageView) view.findViewById(R.id.profile_image);
+        textViewEmail = (TextView) view.findViewById(R.id.email_text);
+        other_info = (TextView) view.findViewById(R.id.other_info);
+        backbtn = view.findViewById(R.id.backbtn);
+
+
+        UserModel userModel = SharedPrefManager.getInstance(getContext()).getUser();
+
+        //setting the values to the textviews
+        textViewUsername.setText(userModel.getFull_name());
+        textViewEmail.setText(userModel.getEmail());
+        other_info.setText(userModel.getUsername() + ", " + userModel.getPhone());
+
+        Glide.with(getContext())
+                .applyDefaultRequestOptions(new RequestOptions()
+                        .placeholder(R.drawable.ic_launcher_background)
+                        .error(R.drawable.ic_launcher_background))
+                .load(userModel.getProfilePic())
+                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)     // cache both original & resized image
+                .centerCrop()
+                .transition(withCrossFade(factory))
+                .into(profile_image);
+
+        //when the user presses logout button
+        //calling the logout method
+        view.findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                new SweetAlertDialog(getContext(), SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("Logout!")
+                        .setContentText("You will be required to Sign in next time")
+                        .setConfirmText("OK")
+                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sDialog) {
+
+                                sDialog.dismissWithAnimation();
+                                db = new LocalStorage(getContext());
+                                db.clearDatabases();
+
+
+                                GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                                        .requestIdToken(getString(R.string.default_web_client_id))
+                                        .requestEmail()
+                                        .build();
+                                GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(getContext(), googleSignInOptions);
+
+                                GoogleSignInAccount gAccount = GoogleSignIn.getLastSignedInAccount(getContext());
+
+                                if (gAccount != null) {
+                                    googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(Task<Void> task) {
+                                            SharedPrefManager.getInstance(getContext()).logout();
+                                        }
+                                    });
+                                } else {
+                                    SharedPrefManager.getInstance(getContext()).logout();
+                                }
+
+                            }
+                        }).show();
+
+            }
+        });
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).popBackStack();
+            }
+        });
+
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // network register and start background serice for network status
+        LocalBroadcastManager.getInstance(getContext()).registerReceiver(new NetworkReceiver(), new IntentFilter(NetworkStatusIntentService.ACTION_NETWORK_STATUS));
+        Intent networkIntent = new Intent(getContext(), NetworkStatusIntentService.class);
+        getContext().startService(networkIntent);
+
+
+
     }
 }
