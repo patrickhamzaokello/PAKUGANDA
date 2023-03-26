cf1cbce14e08dd66a22628d330b503359b5f4c2a
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2023-03-26 18:59:09 +0300

    updates

--- app/src/main/java/com/pkasemer/pakuganda/Fragments/Home.java
+++ app/src/main/java/com/pkasemer/pakuganda/Fragments/Home.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.pkasemer.pakuganda.R;
 
@@ -15,6 +18,7 @@ public class Home extends Fragment {
 
     private static final int PAGE_START = 1;
 
+    LinearLayout viewMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,7 +33,16 @@ public class Home extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        viewMap = view.findViewById(R.id.viewmapbtn);
 
+        viewMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController navController = NavHostFragment.findNavController(requireParentFragment());
+                navController.navigate(R.id.action_navigation_home_to_navigation_map);
+
+            }
+        });
         return view;
     }
 
